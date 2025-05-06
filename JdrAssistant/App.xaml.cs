6c52330115d1002af34dd10d0b166e56fb80aafe
Patrick Sardinha <patricksardinha.pro@gmail.com>
2025-05-06 15:18:49 +0200

    Ajoutez des fichiers projet.

---
+++ JdrAssistant/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace JdrAssistant
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
