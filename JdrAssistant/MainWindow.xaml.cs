785c32bf7b99cf8b4de193ab9007ab36f7350c1b
Patrick Sardinha <patricksardinha.pro@gmail.com>
2025-05-06 16:31:24 +0200

    react integration

--- JdrAssistant/MainWindow.xaml.cs
+++ JdrAssistant/MainWindow.xaml.cs
@@ -1,24 +1,29 @@
-﻿using System.Text;
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Wpf;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace JdrAssistant
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            InitAsync();
+        }
+
+        private async void InitAsync()
+        {
+            // For production mode we will copy the react build in /dist inside a wwwroot folder and point on index.html
+            //var webViewPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
+
+            // For development mode we just point on the local server
+            var webViewPath = "http://localhost:5173";
+
+            await webView.EnsureCoreWebView2Async();
+            webView.Source = new Uri(webViewPath);
         }
     }
 }
